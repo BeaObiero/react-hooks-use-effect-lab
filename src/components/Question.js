c2b160bd47b05f65f1fb345d6d1913ebcdf6b400
Bea Obiero <bea.obiero@gmail.comgit config --global user.email bea.obiero@gmail.comgit config --global user.email + + bea.obiero@gmail.com>
2024-05-29 00:30:12 -0700

    Completed assignment

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,17 +1,30 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
   // add useEffect code
+  useEffect(() => {
+    if(timeRemaining >0){ //checks if timer is greater than 0, only counts if there is time left
+      const timeoutId = setTimeout(() => {
+        setTimeRemaining(timeRemaining - 1); //countdown timer decrement
+      }, 1000);
+
+      //sweep up to clean up function
+      return() => clearTimeout(timeoutId);
+    } else {
+      setTimeRemaining(10);
+      onAnswered(false);  
+    }
+  }, [timeRemaining, onAnswered] ); //dependency array ensure the effect runs correctly after change
 
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
-    onAnswered(isCorrect);
+    onAnswered(isCorrect); //resets timer and sends data to parent component
   }
 
   const { id, prompt, answers, correctIndex } = question;
-
+//renders the question, answers and countdown timer
   return (
     <>
       <h1>Question {id}</h1>
